19f4ffaf8bdacf26841b5d8a8cae0f0018273384
alxtpl <alxtpl@gmail.com>
2021-12-24 00:43:26 +0200

    a little part of  second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,39 @@
-import Player from '@vimeo/player';
+// const throttle = require('lodash.throttle');
+// import throttle from 'lodash.throttle';
 
-// const player = new Player('handstick', {
-//     // id: 19231868,
-//     // width: 640,
-// });
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
+player.on('timeupdate', onTime);
+document.addEventListener(
+    'scroll',
+    _.throttle(() => {
+        console.log('Scroll handler call every 300ms');
+    }, 300),
+);
+// var throttled = _.throttle(renewToken, 300000, { trailing: false });
+// jQuery(element).on('click', throttled);
+
+function onTime() {
+    // console.log('played the video!');
+    player
+        .getCurrentTime()
+        .then(function(seconds) {
+            // seconds = the current playback position
+            console.log(seconds);
+        })
+        .catch(function(error) {
+            // an error occurred
+        });
+}
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+console.log(player);
+
+// {
+//   duration: 61.857;
+//   percent: 0.049;
+//   seconds: 3.034;
+// }
