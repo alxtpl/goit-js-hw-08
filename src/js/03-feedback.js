641dece03244efa2a68cf8bad9dc2121c12e56c4
alxtpl <alxtpl@gmail.com>
2021-12-25 00:40:55 +0200

    adding part third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+const inputFormNode = document.querySelector('.feedback-form');
+inputFormNode.addEventListener('input', onFormSubmit);
+const data = {};
 
+function onFormSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    formData.forEach((value, mail) => {
+        data[mail] = value;
+        console.log('data', data);
+    });
+}
